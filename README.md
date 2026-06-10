2f7591fb3c4210683f3039a4189ca9ae4840efa3
Akash Sahoo <sahoo.akash54@gmail.com>
2026-06-10 23:33:49 +0530

    Update project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Expense Tracker
+
+A simple command-line Expense Tracker built with Python. This project helps users manage their daily expenses by allowing them to add, view, delete, and calculate monthly expenses. All expense records are stored in a CSV file, ensuring data persists between sessions.
+
+## Features
+
+* Add new expenses
+* View all expenses
+* Delete expenses
+* Update existing expenses
+* Calculate monthly expense totals
+* Save expenses to CSV
+* Load expenses automatically from CSV on startup
+* Handle missing files and invalid menu inputs
+* Persistent storage using CSV files
+
+
+## Technologies Used
+
+* Python
+* CSV Module
+* File Handling
+* Exception Handling
+
+## Learning Outcomes
+
+This project demonstrates the use of:
+
+* Functions
+* Lists and Dictionaries
+* CSV Reading and Writing
+* Exception Handling
+* CRUD Operations
+* Data Persistence
--- expense_tracker.py
+++ expense_tracker.py
@@ -67,6 +67,42 @@ def delete_expenses():
     else:
         print("Enter a valid row")
 
+def update_expense():
+    view_expenses()
+    if not expenses_record :
+        return
+    try:
+        select_row = int(input("Enter the Sl. no you want to update: "))
+        if 1 <= select_row <= len(expenses_record) :
+            selected = expenses_record[select_row-1]
+            print(selected)
+            print("For update Date enter 1: " )
+            print("For update Category enter 2: ")
+            print("For update Amount enter 3: ")
+            print("For update Note enter 4: ")
+            updater = int(input("Choose field to update: "))
+            if updater == 1 :
+                new_date = input("Enter the new date: ")
+                selected["Date"] = new_date
+                print("Update Successfull")
+            elif updater == 2 :
+                new_category = input("Enter the new category")
+                selected["Category"] = new_category
+                print("Update Successfull")
+            elif updater == 3 :
+                new_amount = int(input("Enter the new amount"))
+                selected["Amount"] = new_amount
+                print("Update Successfull")
+            elif updater == 4 :
+                new_note = input("Enter the new note")
+                selected["Note"] = new_note
+                print("Update Successfull")
+            else:
+                print("Enter a valid option")
+        else:
+            print("Enter a valid option")
+    except ValueError:
+        print("Input a valid option")
 
 read_csv()
 
@@ -76,7 +112,8 @@ while True :
 3. Calcualte monthly total
 4. Save to CSV
 5. Delete expenses
-6. Exit''' )
+6. Update expenses
+7. Exit''' )
 
 
 
@@ -88,6 +125,7 @@ while True :
     
     if choice == 1 :
         add_expenses()
+        save_csv()
     elif choice == 2 :
         view_expenses()
     elif choice == 3 :
@@ -95,8 +133,12 @@ while True :
     elif choice == 4 :
         save_csv()    
     elif choice == 5 :
-        delete_expenses()    
+        delete_expenses()  
+        save_csv()  
     elif choice == 6 :
+        update_expense()
+        save_csv()
+    elif choice == 7 :
         save_csv()
         break
     else:
